998d62380d199d8ebe5888c885c900f05de7b17a
trung.vuong <demonkid98@gmail.com>
2017-01-06 18:05:28 +0100

    refine a-conversion to always replace vars for simplicity

--- ocaml/alpha.ml
+++ ocaml/alpha.ml
@@ -1,45 +1,36 @@
 open KNormal
 
-module ToReplaceVars = Map.Make(String)
+module Vars = Map.Make(String)
 
-let lookup id repl_vars =
-  try ToReplaceVars.find id repl_vars
+let lookup id vars =
+  try Vars.find id vars
   with e -> id
 
-let rec g (exp : t) (vars : string list) (repl_vars : string ToReplaceVars.t) : t =
+let rec g (exp : t) (vars : string Vars.t) : t =
   match exp with
   | Unit -> exp
   | Bool b -> exp
   | Int i -> exp
   | Float f -> exp
   | Add (id1, id2) ->
-    let newid1 = lookup id1 repl_vars in
-    let newid2 = lookup id2 repl_vars in
+    let newid1 = lookup id1 vars in
+    let newid2 = lookup id2 vars in
     Add (newid1, newid2)
   | Sub (id1, id2) ->
-    let newid1 = lookup id1 repl_vars in
-    let newid2 = lookup id2 repl_vars in
+    let newid1 = lookup id1 vars in
+    let newid2 = lookup id2 vars in
     Sub (newid1, newid2)
   | Let ((id, t), e1, e2) ->
-    if List.mem id vars then
-      let newid = Id.genid () in
-      let vars = vars @ [newid] in
-      let repl_vars = ToReplaceVars.add id newid repl_vars in
-      Let (
-        (newid, t),
-        g e1 vars repl_vars,
-        g e2 vars repl_vars
-      )
-    else
-      let vars = vars @ [id] in
-      Let (
-        (id, t),
-        g e1 vars repl_vars,
-        g e2 vars repl_vars
-      )
+    let newid = Id.genid () in
+    let new_vars = Vars.add id newid vars in
+    Let (
+      (newid, t),
+      g e1 vars,
+      g e2 new_vars
+    )
   | Var id ->
-    let newid = lookup id repl_vars
+    let newid = lookup id vars
     in Var (newid)
   | _ -> failwith "undef"
 
-let rec f (exp : t) : t = g exp [] ToReplaceVars.empty
+let rec f (exp : t) : t = g exp Vars.empty
--- ocaml/tests/alpha_test.ml
+++ ocaml/tests/alpha_test.ml
@@ -1,26 +1,9 @@
 open KNormal
 
-(* simple let, no variable replacement expected *)
-let case1 () =
-  print_endline ">> case 1";
-  (* let x = 1 in x *)
-  let knormed =
-    Let (
-      (
-        "x",
-        Type.Var (ref (Some Type.Int))
-      ),
-      Int 1,
-      Var "x"
-    ) in
-  let alpha_ed = Alpha.f knormed in
-  print_endline (KNormal.to_s alpha_ed);
-  assert (alpha_ed = knormed)
-
-(* nested let with same name variables, variable replacement expected *)
+(* variable replacement expected if declared in let *)
 let case2 () =
   print_endline ">> case 2";
-  (* let x = 1 in (let _ = (let x = 2. in x) in x) *)
+  (* let x = 1 in (let _ = (let x = 2. in x + y) in x) *)
   let knormed =
     Let (
       (
@@ -40,7 +23,10 @@ let case2 () =
             Type.Var (ref (Some Type.Float))
           ),
           Float 2.,
-          Var "x"
+          Add (
+            "x",
+            "y"
+          )
         ),
 
         Var "x"
@@ -51,5 +37,4 @@ let case2 () =
   assert (not (alpha_ed = knormed))
 
 let () =
-  case1 ();
   case2 ()
