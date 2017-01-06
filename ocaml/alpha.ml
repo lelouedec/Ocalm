f1a790883eedcede37ecec92d7d37c609e8146d6
trung.vuong <demonkid98@gmail.com>
2017-01-06 16:03:58 +0100

    implement alpha-conversion on Let (var) expression (wip)

--- ocaml/alpha.ml
+++ ocaml/alpha.ml
@@ -1,17 +1,32 @@
 open KNormal
 
-let rec f (exp : t) : t =
+module ToReplaceVars = Map.Make(String)
+
+
+let rec g (exp : t) (vars : string list) (repl_vars : string ToReplaceVars.t) : t =
   match exp with
   | Unit -> exp
   | Bool b -> exp
   | Int i -> exp
   | Float f -> exp
   | Let ((id, t), e1, e2) ->
-    Let (
-      (Id.genid (), t),
-      f e1,
-      f e2)
-  | Var id -> Var (Id.genid ())
+    if List.mem id vars then
+      let newid = Id.genid () in
+      let vars = vars @ [newid] in
+      let repl_vars = ToReplaceVars.add id newid repl_vars in
+      Let (
+        (newid, t),
+        g e1 vars repl_vars,
+        g e2 vars repl_vars
+      )
+    else
+      let vars = vars @ [id] in
+      Let (
+        (id, t),
+        g e1 vars repl_vars,
+        g e2 vars repl_vars
+      )
+  | Var id -> Var id
   | _ -> failwith "undef"
 
-let g () : t = Var("x")
+let rec f (exp : t) : t = g exp [] ToReplaceVars.empty
--- ocaml/tests/alpha_test.ml
+++ ocaml/tests/alpha_test.ml
@@ -1,16 +1,51 @@
+open KNormal
+
+(* simple let, no variable replacement expected *)
 let case1 () =
   (* let x = 1 in x *)
   let knormed =
-    KNormal.Let (
+    Let (
       (
         "x",
         Type.Var (ref (Some Type.Int))
       ),
-      KNormal.Int(1),
-      KNormal.Var("x")
+      Int 1,
+      Var "x"
     ) in
   let alpha_ed = Alpha.f knormed in
   assert (alpha_ed = knormed)
 
+(* nested let with same name variables, variable replacement expected *)
+let case2 () =
+  (* let x = 1 in (let _ = (let x = 2. in x) in x) *)
+  let knormed =
+    Let (
+      (
+        "x",
+        Type.Var (ref (Some Type.Int))
+      ),
+      Int 1,
+      Let (
+        (
+          "_",
+          Type.Var (ref (Some Type.Unit))
+        ),
+
+        Let (
+          (
+            "x",
+            Type.Var (ref (Some Type.Float))
+          ),
+          Float 2.,
+          Var "x"
+        ),
+
+        Var "x"
+      )
+    ) in
+  let alpha_ed = Alpha.f knormed in
+  assert (not (alpha_ed = knormed))
+
 let () =
-  case1 ()
+  case1 ();
+  case2 ()
