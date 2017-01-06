af9abc4f778ec351b4ed910b2b8d8bc698d7b602
lelouedec <lelouedec@lelouedec-ThinkPad-L460>
2017-01-06 16:51:28 +0100

    data type for asml started

--- ocaml/asml.ml
+++ ocaml/asml.ml
@@ -1,16 +1,38 @@
-type id_or_imm = V of id.t | C of int 
+type ident_or_imm = 
+	| Ident of Id.t 
+	| Int of int 
 
-type t = 
-	| Let of (Id.t * Type.t)  * exp * t
-	| Forget of Id.t * t 
-and exp = 
-	| Add of Id.t * id_or_imm
-	| Ld of mem( x + y )
-	| St of mem( x + y ) <- z
-	| FAddD of Id.t * Id.t
-	| IfEq of Id.t * id_or_imm * t * t
-	| IfFEq of Id.t * Id.t * t * t
-	| CallCls of Id.t * Id.t list * Id.t list
-	| CallDir of Id.l * Id.t list * Id.t list
-	| Save of Id.t * Id.t
-	| Restore of Id.t
+type exp = 
+	| Nop 
+	| LPexpRp of ( exp ) 
+	| Int 
+	| Label 
+	| Neg of Id.t
+	| FNeg of Id.t
+	| Add of Id.t * ident_or_imm
+	| Sub of Id.t * ident_or_imm
+	(*| Ld of Mem
+	| St of Mem2*)	
+	| FAdd of Id.t * Id.t
+	| FSub of Id.t * Id.t
+	| FMul of Id.t * Id.t 
+	| FDiv of Id.t * Id.t 
+	| New of ident_or_imm
+	| Mem of Id.t * ident_or_imm
+	| Mem2 of  Id.t * ident_or_imm * Id.t 
+	| IfEq of Id.t * ident_or_imm * asmt * asmt
+	| IfLEq of Id.t * ident_or_imm * asmt * asmt
+	| IfGEq of Id.t * ident_or_imm * asmt * asmt
+	| CallLabel of formal_args
+	| CallClo of Id.t * formal_args
+and asmt =
+	| LPasmRp of (asmt)
+	| LetIn of Id.t * exp * asmt
+and formal_args = 
+	| Ident of formal_args
+	| Nil
+and fundefs = 
+ 	| Let_ of asmt
+ 	| LetLabeleqFloat of  fundefs
+ 	| LetLaelEq of formal_args * asmt * fundefs
+
