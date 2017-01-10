146ae2e341331c39fab05126012b9c171bbee800
trung.vuong <demonkid98@gmail.com>
2017-01-10 14:39:56 +0100

    apply closure-conversion for direct fn call and test with to_string

--- ocaml/tests/closure_test.ml
+++ ocaml/tests/closure_test.ml
@@ -12,8 +12,31 @@ let case1 () =
       )
     ) in
   let closured = f knormed in
+  print_endline ">> case 1";
+  print_endline (to_string closured);
+  ()
+
+let case2 () =
+  (* let rec double x = x + x in (let y = 1 in double y) *)
+  let knormed =
+    KNormal.LetRec (
+      {
+        name = ("double", Type.Int);
+        args = [("x", Type.Int)];
+        body = KNormal.Add ("x", "x")
+      },
+      KNormal.Let (
+        ("y", Type.Int),
+        Int 1,
+        KNormal.App ("double", ["y"])
+      )
+    ) in
+  let closured = f knormed in
+  print_endline ">> case 2";
+  print_endline (to_string closured);
   ()
 
 let () =
-  print_endline "Closure conversion tests";
+  print_endline "\n*****\nClosure conversion tests";
   case1 ();
+  case2 ();
