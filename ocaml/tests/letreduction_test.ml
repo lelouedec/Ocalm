af36ebf224bd45b3dda0ffceec3ddc4521f912ea
Anh Tho Le <leanh@f211-14.e-im2ag.ujf-grenoble.fr>
2017-01-11 14:48:00 +0100

    nested let reduction added

---
+++ ocaml/tests/letreduction_test.ml
@@ -0,0 +1,33 @@
+open KNormal
+
+(* let x = (let y = x + y in x2) in x + x2  *)
+let case1 () = 
+	print_endline ">> case 1";
+	let knormed = 
+		Let (
+			(
+				"x",
+				Type.Var (ref (Some Type.Float))
+			),
+			Let (
+				(
+					"y",
+					Type.Var (ref (Some Type.Int))
+				),
+				Add (
+          "x",
+          "y"
+        ),
+        Var "x2"
+			),
+			Add (
+        "x",
+        "x3"
+      )
+		) in
+	let letreduction_ed = Letreduction.f knormed in
+	print_endline (KNormal.to_string letreduction_ed)
+
+let () =
+  print_endline "Nested let reduction tests";
+  case1 ()
