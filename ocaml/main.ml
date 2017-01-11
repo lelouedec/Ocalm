fdb17f9e785d7869a4869e1f316591b29311d5f8
ahmedzr <ahmedzrigui01@gmail.com>
2017-01-11 00:21:41 +0000

    Add files via upload
    Some basic tests in main

--- ocaml/main.ml
+++ ocaml/main.ml
@@ -24,33 +24,22 @@ let exp2 =
     LetLabelEq ("f",["x";"y"], Exp( Add( "x" , Int (0) ) ), LetUnderscEQ(Exp ( CallLabel ( "f") ) )) in 
 Asml.test exp2 ;
 
+
+
+
 let exp3 =
-    LetLabelEq ("succ",["x"], LetIdentEq("t",Neg("1"),Exp( Add( "x" , Int(1)) )),  (* x +t *)
-     LetUnderscEQ(LetIdentEq("y",Neg("1"),Exp (CallLabel("succ y")) )))   in  (*pb succ*)
+    LetLabelEq ("f",["x"], LetIdentEq("z",Add( "x" , Int(1)),Exp( Sub( "z" , Int(2)) )),  (* x +t *)
+     LetUnderscEQ(Exp ( CallLabel ( "f") )))   in  (*pb succ*)
 Asml.test exp3 ;
 
 let exp4 =
-    LetLabelEq ("f",["x"], LetIdentEq("z",Neg("1"),Exp( Add( "x" , Int(1)) )),  (* x +t *)
+    LetLabelEq ("succ",["x"], LetIdentEq("t",Neg("1"),Exp( Add( "x" , Int(1)) )),  (* x +t *)
      LetUnderscEQ(LetIdentEq("y",Neg("1"),Exp (CallLabel("succ y")) )))   in  (*pb succ*)
 Asml.test exp4 ;
 
+
 Register_alloc.allocate exp2;
-Register_alloc.allocate exp3;
+(*Register_alloc.allocate exp3; *)
 
 
 
-
-(*
-let test () =
-  print_endline "Test"; 
-  let exp2 =
-    LetLabelEq ("f",Ident "x", Exp( Add( "x" , Int (0) ) ), LetUnderscEQ(Exp ( CallLabel ( "f") ) )) in print_endline (fundefs_to_string (exp2) );
-    let exp3 =
-            (*LetLabelEq ("succ",Ident "x", Exp( Add( "x" , Int (0) ) ), LetUnderscEQ(Exp ( CallLabel ( "f") ) )) in print_endline (fundefs_to_string (exp3) );*)
-         LetLabelEq("succ",Ident "x",LetIdentEq("t",Neg ("1"),Exp( Add( "x" ,Ident "t" ) )),
-          LetUnderscEQ(Exp ( CallLabel("succ")))) in print_endline (fundefs_to_string (exp3));                                     
-     let exp4 = 
-      LetUnderscEQ(LetIdentEq ("x", Neg ("1"), Exp (Add( "x" , Int (0) ) ))) in print_endline (fundefs_to_string (exp4));
-     let exp5 = 
-      LetLabelEq(,,,);
-*)
