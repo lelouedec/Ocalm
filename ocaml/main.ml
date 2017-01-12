063480132c3c5acfa527e06615917a5b03ce988f
ahmedzr <ahmedzrigui01@gmail.com>
2017-01-12 23:04:42 +0100

    final change on call label so we can put integer as parameter and va..
    final change on call label so we can put integer as parameter and va…

--- ocaml/main.ml
+++ ocaml/main.ml
@@ -46,3 +46,17 @@ let exp5 =
 Asml.test exp5 ;
 let h3 = Register_alloc.allocate exp5 in () ;
 
+let exp6 =
+    LetLabelEq (
+      "Sum",
+      ["x";"y";"z"],
+      LetIdentEq("a",Add( "x" , Ident "y"),Exp (Add( "a" , Ident "z"))),
+        LetLabelEq(
+          "Diff",
+          ["x";"y"],
+          Exp (Sub("x",Ident "y")),
+            LetUnderscEQ (LetIdentEq("a",Neg("10"),
+              LetIdentEq ("x",Neg("1"),LetIdentEq("y",Neg("2"),LetIdentEq("z",Neg("3"),
+                LetIdentEq("u",CallLabel ( "f",[Ident"x";Ident "y"]),Exp ( CallLabel ( "diff",[Ident"a";Ident "u"]) ) ) ) )))))) in 
+Asml.test exp6 ;
+let h4 = Register_alloc.allocate exp6 in () ;
