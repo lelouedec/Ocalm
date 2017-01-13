14d5fb3373bf9c26aca4de15cefa10501672d7e1
trung.vuong <demonkid98@gmail.com>
2017-01-13 11:52:35 +0100

    add system test for typing

---
+++ scripts/mincaml-test-typing.sh
@@ -0,0 +1,35 @@
+#! /bin/sh
+cd "$(dirname "$0")"/.. || exit 1
+
+# TODO change this to point to your mincamlc executable if it's different, or add
+# it to your PATH. Use the appropriate option to run the parser as soon
+# as it is implemented
+MINCAMLC=ocaml/mincamlc
+
+# run all test cases in syntax/valid and make sure they are parsed without error
+# run all test cases in syntax/invalid and make sure the parser returns an error
+
+# TODO extends this script to run test in subdirectories
+# 
+
+for test_case in tests/typechecking/valid/*.ml
+do
+    echo "testing type checking on: $test_case"
+    if $MINCAMLC -t "$test_case" 2> /dev/null 1> /dev/null
+    then
+        echo "OK"
+    else 
+        echo "KO"
+    fi
+done
+
+for test_case in tests/typechecking/invalid/*.ml
+do
+    echo "testing type checking on: $test_case"
+    if $MINCAMLC -t "$test_case" 2> /dev/null 1> /dev/null
+    then
+        echo "OK"
+    else 
+        echo "KO"
+    fi
+done
