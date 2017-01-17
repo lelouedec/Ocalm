b935b51990e0baa231a622abacc919acfc3a3351
trung.vuong <demonkid98@gmail.com>
2017-01-17 16:31:08 +0100

    update test runners

--- scripts/mincaml-test-typing.sh
+++ scripts/mincaml-test-typing.sh
@@ -2,16 +2,20 @@
 cd "$(dirname "$0")"/.. || exit 1
 
 MINCAMLC=ocaml/mincamlc
+nb_cases=0
+nb_failures=0
 
 echo "--------------------"
 echo "testing valid scripts"
 for test_case in tests/typechecking/valid/*.ml
 do
+    nb_cases=$((nb_cases + 1))
     echo "testing type checking on: $test_case"
     if $MINCAMLC -t "$test_case" 2> /dev/null 1> /dev/null
     then
         echo "OK"
-    else 
+    else
+        nb_failures=$((nb_failures + 1))
         echo "KO ~~~~~~~~~~ OK expected!!!"
     fi
 done
@@ -20,11 +24,21 @@ echo "--------------------"
 echo "testing invalid scripts"
 for test_case in tests/typechecking/invalid/*.ml
 do
+    nb_cases=$((nb_cases + 1))
     echo "testing type checking on: $test_case"
     if $MINCAMLC -t "$test_case" 2> /dev/null 1> /dev/null
     then
+        nb_failures=$((nb_failures + 1))
         echo "OK ~~~~~~~~~~ KO expected!!!"
     else 
         echo "KO"
     fi
 done
+
+echo "run $nb_cases tests"
+if test $nb_failures -gt $((0))
+then
+    echo "$nb_failures/$nb_cases tests failed!"
+else
+    echo "all passed!"
+fi
---
+++ tests/typechecking/valid/same_vars_diff_fn.ml
@@ -0,0 +1,5 @@
+let rec f x = 1 + x in
+let rec g x = 2. *. x in
+let x = f 10 in
+let x = g 1. in
+()
