67ada3453ea350d02895fc5da9c723185af116ec
trung.vuong <demonkid98@gmail.com>
2017-01-18 16:58:40 +0100

    add closure env loading to asml function body generation
    free vars for closure are loaded from heap

--- ocaml/virtual.ml
+++ ocaml/virtual.ml
@@ -27,7 +27,24 @@ let rec tr cls =
   (*| Float f -> *)
   | Let ((id, t), e1, e2) -> Asml.LetIdentEq (id, make_exp e1, tr e2)
   | Var id -> Asml.Exp (make_exp cls)
+  | AppCls (id, args) -> Asml.Exp (make_exp cls)
+  | AppDir (id, args) -> Asml.Exp (make_exp cls)
   | _ -> Asml.Exp (make_exp cls)
+
+let rec generate_fun_body fargs ?(counter = 1) fbody =
+  let word_size = 4 in
+  match fargs with
+  | [] -> tr fbody
+  | hd :: tl ->
+    (* function with free vars -- closure,
+      have to load env values from heap to assign free vars *)
+    let x = fst hd in
+    Asml.LetIdentEq (
+      x,
+      Asml.Ld ("%self", Asml.Int (counter * word_size)),
+      generate_fun_body tl ~counter:(counter + 1) fbody
+    )
+
 
 let f cls =
   let cls_fns, cls_main = cls in
@@ -36,10 +53,9 @@ let f cls =
     | [] -> Asml.LetUnderscEQ (tr cls_main)
     | hd :: tl ->
       (* assume no float function *)
-      let ((fname, ftype), args, fargs, body) = hd in
+      let ((fname, ftype), args, fargs, fbody) = hd in
       let asml_args =
-        (List.map fst args)
-        @ (List.map fst fargs) in
-      let asml_body = tr body in
+        (List.map fst args) in
+      let asml_body = generate_fun_body fargs fbody in
       Asml.LetLabelEq (fname, asml_args, asml_body, transform tl) in
   transform cls_fns
