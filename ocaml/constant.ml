69416f9dbf8157c26f5255c93a6f4dfcffa94665
Anh Tho Le <anhtholee93@gmail.com>
2017-01-19 16:50:11 +0100

    minor changes for let.ml and constant.ml

--- ocaml/constant.ml
+++ ocaml/constant.ml
@@ -3,52 +3,72 @@ open KNormal
 (* check identifier type for int, float or tuple  *)
 let is_int id vars =
   try (
-    match St.find id vars with 
-    | Int _ -> true 
-    | _ -> false
+    let type_int = St.find id vars
+    in ( 
+      match type_int with 
+      | Int _ -> true 
+      | _ -> false
+    )
   )
   with e -> false
   
 let is_float id vars =
   try (
-    match St.find id vars with 
-    | Float _ -> true 
-    | _ -> false
+    let type_float = St.find id vars
+    in ( 
+      match type_float with 
+      | Float _ -> true 
+      | _ -> false
+    )
   )
   with e -> false
 
 let is_tuple id vars =
   try (
-    match St.find id vars with 
-    | Tuple _  -> true 
-    | _ -> false
+    let type_tuple = St.find id vars
+    in ( 
+      match type_tuple with 
+      | Tuple _  -> true 
+      | _ -> false
+    )
   )
   with e -> false
 
 (* Find variable value in the mapping *)
 let find_int id vars = 
-  match St.find id vars with 
-  | Int i -> i 
-  | _ -> failwith "Value not found"
-
+  let val_int = St.find id vars 
+  in (
+    match val_int with 
+    | Int i -> i 
+    | _ -> failwith "Value not found"
+  )
+  
 let find_float id vars = 
-  match St.find id vars with 
-  | Float f -> f 
-  | _ -> failwith "Value not found"
+  let val_float = St.find id vars 
+  in (
+    match val_float with 
+    | Float f -> f 
+    | _ -> failwith "Value not found"
+  )
+  
 
 let find_tuple id vars = 
-  match St.find id vars with 
-  | Tuple l -> l 
-  | _ -> failwith "Value not found"
+  let val_tuple = St.find id vars
+  in (
+    match val_tuple with 
+    | Tuple l -> l 
+    | _ -> failwith "Value not found"
+  )
+  
 
 let rec g exp vars = 
   match exp with 
   | Add (id1, id2) when is_int id1 vars && is_int id2 vars -> Int (find_int id1 vars + find_int id2 vars)
   | Sub (id1, id2) when is_int id1 vars && is_int id2 vars -> Int (find_int id1 vars - find_int id2 vars)
-  | Neg id when is_int id vars -> Int(-(find_int id vars))
+  | Neg id when is_int id vars -> Int (-(find_int id vars))
   | FAdd (id1, id2) when is_float id1 vars && is_float id2 vars -> Float (find_float id1 vars +. find_float id2 vars)
   | FSub (id1, id2) when is_float id1 vars && is_float id2 vars -> Float (find_float id1 vars -. find_float id2 vars)
-  | FNeg x when is_float x vars -> Float(-.(find_float x vars))
+  | FNeg x when is_float x vars -> Float (-.(find_float x vars))
   | FMul (id1, id2) when is_float id1 vars && is_float id2 vars -> Float (find_float id1 vars *. find_float id2 vars)
   | FDiv (id1, id2) when is_float id1 vars && is_float id2 vars -> Float (find_float id1 vars /. find_float id2 vars)
   | Var id when is_int id vars -> Int (find_int id vars)
@@ -58,21 +78,12 @@ let rec g exp vars =
   | IfEq (id1, id2, e1, e2) when is_float id1 vars && is_float id2 vars -> if find_float id1 vars = find_float id2 vars then g e1 vars else g e2 vars
   | IfLE (id1, id2, e1, e2) when is_int id1 vars && is_int id2 vars -> if find_int id1 vars <= find_int id2 vars then g e1 vars else g e2 vars
   | IfLE (id1, id2, e1, e2) when is_float id1 vars && is_float id2 vars -> if find_float id1 vars <= find_float id2 vars then g e1 vars else g e2 vars
-  | IfEq (id1, id2, e1, e2) -> IfEq (id1, id2, g e1 vars, g e2 vars)
-  | IfLE (id1, id2, e1, e2) -> IfLE (id1, id2, g e1 vars, g e2 vars)
   | Let ((id, t), e1, e2) -> 
       let new_e1 = g e1 vars in
       let new_e2 = g e2 (St.add id new_e1 vars) in
       Let ((id, t), new_e1, new_e2)
   | LetRec ({ name = (label, t); args = args; body = body }, e) ->
       LetRec ({ name = (label, t); args = args; body = g body vars }, g e vars)
-  (* | LetTuple (l, e1, e2) when is_tuple e1 vars ->
-      List.fold_left2
-        (fun e' xt z -> Let (xt, Var (z), e'))
-        (g e2 vars)
-        l
-        (find_tuple e1 vars)
-  | LetTuple (l, e1, e2) -> LetTuple (l, e1, g e2 vars) *)
   | _ -> exp
 
 let rec f exp = g exp St.empty
--- ocaml/let.ml
+++ ocaml/let.ml
@@ -3,16 +3,14 @@ open KNormal
 let rec f exp = 
 	match exp with
 	| Let ((id, t), e1, e2) ->
-		let rec insert exp2 = 
-			match exp2 with
+		let rec insert exp1 = 
+			match exp1 with
 			| Let (id, e3, e4) -> Let (id, e3, insert e4)
 			| LetRec (fd, e) -> LetRec (fd, insert e)
-			| LetTuple (l, e1, e2) -> LetTuple (l, e1, insert e2)
 			| e -> Let ((id, t), e, f e2) in
 		insert (f e1)
 	| LetRec ({ name = (label, t); args = args; body = body }, e) -> 
 		LetRec ({ name = (label, t); args = args; body = f body }, f e)
-	(* | LetTuple (l, e1, e2) -> LetTuple (l, e1, f e2) *)
 	| IfEq (id1, id2, e1, e2) -> IfEq (id1, id2, f e1, f e2) 
 	| IfLE (id1, id2, e1, e2) -> IfLE (id1, id2, f e1, f e2) 
 	| e -> e
