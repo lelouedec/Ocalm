69325edda00ca02514eea282c36ba2d8e3704eaa
ahmedzr <ahmedzrigui01@gmail.com>
2017-01-19 11:00:19 +0100

    Basic tests code gen
    Basic tests code gen

--- tests/gen-code/If_else.ml
+++ tests/gen-code/If_else.ml
@@ -1,3 +1,4 @@
-let a = 1 and b = 2 in 
+let a = 1 in
+	let b = 2 in 
 if a < b then print_int a 
 		 else print_int b
--- tests/gen-code/more_than_4param.ml
+++ tests/gen-code/more_than_4param.ml
@@ -1,5 +1,5 @@
- 
-(* More than 4 parameters in a function*)
- let sum a b c d e   = (a + b + c + d + e)  in
 
-sum 2 3 4 5 6  
+
+ let rec sum a b c d e f  = a + b + c + d + e + f in 
+	let s= sum 2 3 4 5 6 7 in ()
+
