ba70eb2bd7e8f799b4d35267ea54287108687ea8
ahmedzr <ahmedzrigui01@gmail.com>
2017-01-22 19:46:55 +0100

    Code gen script, not ready yet
    Code gen script, not ready yet

---
+++ scripts/mincaml-test-code-generation.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+echo "hello"
+
+
+nb_cases=0
+nb_failures=0
+
+echo "--------------------"
+echo "testing Code generation"
+cd ../tests/gen-code/
+mkdir Actual
+for test_case in *.ml
+do
+    nb_cases=$((nb_cases + 1))
+    echo "testing Code generation on: $test_case"
+    #compiling
+    #./a > Actual/"$test_case".actual
+    #./a > Expected/"$test_case".expected
+     if (cmp -s Actual/"$test_case".actual Expected/"$test_case".expected)
+     then
+     	echo "OK "
+     else 
+     	nb_failures=$((nb_failures + 1))
+     	echo "KO"
+     fi
+    
+done
+
+echo "run $nb_cases tests"
+if test $nb_failures -gt $((0))
+then
+    echo "$nb_failures/$nb_cases tests failed!"
+else
+    echo "all passed!"
+fi
+
+
