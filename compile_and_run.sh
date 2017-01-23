9ee22994768c1dd0dd2b194324daa57de95ad928
Hugo Frezat <frezath@F211-07.e-im2ag.ujf-grenoble.fr>
2017-01-23 17:58:59 +0100

    Enhance compile_and_run.sh to test the expecting result

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -11,8 +11,14 @@ echo "Compiling $1..."
 $MINCAMLC -asml $1 > $EBI.s
 
 echo "Compiling ARM..."
-make -C ARM/
+make -C ARM/ 2> /dev/null 1> /dev/null
 
 echo "Running program $1..." 
-$EBI.arm
+mincaml=$($EBI.arm)
 
+groundt=$(ocaml $1)
+if [ "$mincaml" = "$groundt" ]; then
+  echo "Program correct"
+else
+  echo "Program incorrect" 
+fi
