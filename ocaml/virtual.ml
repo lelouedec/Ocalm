c8626dbbec2579625d6383186f8bbd2ee9cbba82
trung.vuong <demonkid98@gmail.com>
2017-01-24 11:11:52 +0100

    fix closure conversion/virtualization when a function passes itself as free var for other closure

--- ocaml/virtual.ml
+++ ocaml/virtual.ml
@@ -23,25 +23,47 @@ let rec make_exp cls =
   | AppDir (id, args) -> Asml.CallLabel(id, args)
   | _ -> Asml.Nop
 
-let rec tr cls = 
+let rec tr cls current_fn =
   match cls with
   | Unit -> Asml.Exp (Asml.Nop)
   (*| Float f -> *)
-  | IfEq (id1, id2, e1, e2) -> Asml.Exp (Asml.IfEq(id1, Asml.Ident(id2), tr e1, tr e2))
-  | IfLE (id1, id2, e1, e2) -> Asml.Exp (Asml.IfLEq(id1, Asml.Ident(id2), tr e1, tr e2))
-  | Let ((id, t), e1, e2) -> Asml.LetIdentEq (id, make_exp e1, tr e2)
+  | IfEq (id1, id2, e1, e2) -> Asml.Exp (Asml.IfEq(id1, Asml.Ident(id2), tr e1 current_fn, tr e2 current_fn))
+  | IfLE (id1, id2, e1, e2) -> Asml.Exp (Asml.IfLEq(id1, Asml.Ident(id2), tr e1 current_fn, tr e2 current_fn))
+  | Let ((id, t), e1, e2) -> Asml.LetIdentEq (id, make_exp e1, tr e2 current_fn)
   | MakeCls ((id, t), label, free_vars, e) ->
     let size = List.length free_vars in
     let addr_id = "addr_" ^ label in
     let rec alloc_mem_for_free_vars free_vars ?(counter = 1) fbody =
       match free_vars with
-      | [] -> tr fbody
+      | [] -> tr fbody current_fn
       | hd :: tl ->
-        Asml.LetIdentEq (
-          Id.gen_asml_id (),
-          Asml.St (id, Asml.Int (counter * word_size), hd),
-          alloc_mem_for_free_vars tl ~counter:(counter + 1) fbody
-        ) in
+        (* TODO a more general case: any function label as argument for others *)
+        if hd = "%self" then (
+          let newid = Id.gen_asml_id () in
+          let newaddr = "addr_" ^ newid in
+          Asml.LetIdentEq (
+            newid,
+            Asml.New (Asml.Int word_size),
+            Asml.LetIdentEq (
+              newaddr,
+              Asml.Label (current_fn),
+              Asml.LetIdentEq (
+                Id.gen_asml_id (),
+                Asml.St (newid, Asml.Int 0, newaddr),
+                Asml.LetIdentEq (
+                  Id.gen_asml_id (),
+                  Asml.St (id, Asml.Int (counter * word_size), newid),
+                  alloc_mem_for_free_vars tl ~counter:(counter + 1) fbody
+                )
+              )
+            )
+          )  
+        ) else
+          Asml.LetIdentEq (
+            Id.gen_asml_id (),
+            Asml.St (id, Asml.Int (counter * word_size), hd),
+            alloc_mem_for_free_vars tl ~counter:(counter + 1) fbody
+          ) in
 
     Asml.LetIdentEq (
       id,
@@ -58,9 +80,9 @@ let rec tr cls =
     )
   | _ -> Asml.Exp (make_exp cls)
 
-let rec generate_fun_body fargs ?(counter = 1) fbody =
+let rec generate_fun_body fname fargs ?(counter = 1) fbody =
   match fargs with
-  | [] -> tr fbody
+  | [] -> tr fbody fname
   | hd :: tl ->
     (* function with free vars -- closure,
       have to load env values from heap to assign free vars *)
@@ -68,7 +90,7 @@ let rec generate_fun_body fargs ?(counter = 1) fbody =
     Asml.LetIdentEq (
       x,
       Asml.Ld ("%self", Asml.Int (counter * word_size)),
-      generate_fun_body tl ~counter:(counter + 1) fbody
+      generate_fun_body fname tl ~counter:(counter + 1) fbody
     )
 
 
@@ -76,12 +98,12 @@ let f cls =
   let cls_fns, cls_main = cls in
   let rec transform functions =
     match functions with
-    | [] -> Asml.LetUnderscEQ (tr cls_main)
+    | [] -> Asml.LetUnderscEQ (tr cls_main "")
     | hd :: tl ->
       (* assume no float function *)
       let ((fname, ftype), args, fargs, fbody) = hd in
       let asml_args =
         (List.map fst args) in
-      let asml_body = generate_fun_body fargs fbody in
+      let asml_body = generate_fun_body fname fargs fbody in
       Asml.LetLabelEq (fname, asml_args, asml_body, transform tl) in
   transform cls_fns
