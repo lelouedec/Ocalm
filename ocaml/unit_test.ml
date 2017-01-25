10f4d57c61afce0a9394ef4faab7563e60c0d5c4
ahmedzr <ahmedzrigui01@gmail.com>
2017-01-25 10:27:53 +0100

    uT

--- ocaml/unit_test.ml
+++ ocaml/unit_test.ml
@@ -2,7 +2,7 @@ open OUnit2;;
 open Printf
 open Asml
 
-  let function2 =
+  let function1 =
     LetLabelEq (
       "Sum",
       ["x";"y";"z";"p1";"p2";"p3";"p4";"p5";"p6";"p7";"p8";"p9"],
@@ -17,77 +17,60 @@ open Asml
 
 
 
-  let function3=
-     LetLabelEq (
-      "succ" ,
-      ["a"; "b"; "c"; "d"; "e"; "f"; "j"; "h"; "i"; "j";"k";"l";"m"], 
-      LetIdentEq (
-        "t" , 
-        Neg ("1") ,
-        Exp( Add ( "x" , Ident "t" ) ) 
-      ),  (* x +t *)
-      LetUnderscEQ (
-        LetIdentEq ("y",
-          Neg ("1"),
-          Exp (
-            CallLabel(
-              "succ ",
-              ["x" ;"y"]
-            )
-          ) 
-        )
-      )
-    )  
+  
 
-
-   let function1 = 
-  LetLabelEq("f",["x";"p1";"p2";"p3";"p4";"p5";"p6";"p7";"p8"],
-            Exp(
-                IfEq("x",
-                      Int(0),
-                      Exp(Int(0)),
-                      LetIdentEq("x", Sub("x",Int(1)),  LetIdentEq("x",Add("x",Ident"x"),Exp(CallLabel("f",["x"]))))
-                )
-            ),
-            LetUnderscEQ(
-                        LetIdentEq("z",
-                          Int(4),
-                          Exp(CallLabel("f",["z";"p1";"p2";"p3";"p4";"p5";"p6";"p7";"p8"]))
-                        )
-                        ) 
-            )
 
 
 
 (***************Testing function 1*******************)
-let function_has = Register_alloc.allocate function2;;
+let function_has = Register_alloc.allocate function1;;
 (*Testing Number of functions*)
 let actual_function_number=function_has#statistics.num_bindings;;
-let test1 test_ctxt = assert_equal 3 (actual_function_number);;
+let test1 test_ctxt = assert_equal ~msg:"Wrong number of functions stored in hashtable" 3 (actual_function_number);;
 
 (*testing the nb of variables  per function*)
-  (*Function f*)
+  (*Function Sum*)
 let fVariables =ref 0;;
 let fu = function_has#look_for "Sum" ;;
 Hashtbl.iter(fun key value -> fVariables:=!fVariables+1  )fu#get_hast;;
-let test2 test_ctxt = assert_equal 13 (!fVariables);;
+let test2 test_ctxt = assert_equal ~msg:"Wrong number of variables stored in the hashtable for function Sum" 13 (!fVariables);;
 
 (*Testing the nubmer of variables allocated in registers and numbers of variables in the stack*)
 let register_variable =ref 0;;
 let stack_variable =ref 0;;
 
-
-
   (*Variables on registers & stack*)
 print_string "function Sum \n";;
 Hashtbl.iter(fun key value -> if (value#get_is_in_stack =0) then register_variable:=!register_variable+1 else  stack_variable:=!stack_variable+1 )fu#get_hast;;
-let test3 test_ctxt = assert_equal 10 (!register_variable);;
-let test4 test_ctxt = assert_equal ~msg:"Er" 3 (!stack_variable);;
+let test3 test_ctxt = assert_equal ~msg:"Wrong number of variables in the stack in function Sum"10 (!register_variable);;
+let test4 test_ctxt = assert_equal ~msg:"Wrong number of variables in the stack in function Sum" 3 (!stack_variable);;
 
 Hashtbl.iter(fun key value ->  print_string key ; print_string " "; print_string value#get_reg ; print_string " " ; print_int value#get_is_in_stack ;  print_string " Time slice    " ; value#display_timeslice ;  print_string "\n")fu#get_hast;;
-(*
-Hashtbl.iter(fun key value ->  print_string key ; print_string " " ; print_string " " ; print_int value#get_is_in_stack ;  print_string "\n" )fu#get_hast;;
-*)
+
+
+
+(*Function Diff*)
+let fVariables =ref 0;;
+let fu = function_has#look_for "Diff" ;;
+Hashtbl.iter(fun key value -> fVariables:=!fVariables+1  )fu#get_hast;;
+let test8 test_ctxt = assert_equal ~msg:"Wrong number of variables stored in the hashtable for function Diff" 2(!fVariables);;
+
+(*Testing the nubmer of variables allocated in registers and numbers of variables in the stack*)
+let register_variable =ref 0;;
+let stack_variable =ref 0;;
+
+  (*Variables on registers & stack*)
+print_string "function Diff \n";;
+Hashtbl.iter(fun key value -> if (value#get_is_in_stack =0) then register_variable:=!register_variable+1 else  stack_variable:=!stack_variable+1 )fu#get_hast;;
+let test9 test_ctxt = assert_equal ~msg:"Wrong number of variables in registers in function Diff" 2  (!register_variable);;
+let test10 test_ctxt = assert_equal ~msg:"Wrong number of variables in the stack in function Diff" 0 (!stack_variable);;
+
+Hashtbl.iter(fun key value ->  print_string key ; print_string " "; print_string value#get_reg ; print_string " " ; print_int value#get_is_in_stack ;  print_string " Time slice    " ; value#display_timeslice ;  print_string "\n")fu#get_hast;;
+
+
+
+
+
 
 (*Function main*)
 let mainVariables =ref 0;;
@@ -105,7 +88,6 @@ let test6 test_ctxt = assert_equal 5 (!register_variable);;
 let test7 test_ctxt = assert_equal ~msg:"Er" 0 (!stack_variable);;
 
 Hashtbl.iter(fun key value ->  print_string key ; print_string " "; print_string value#get_reg ; print_string " " ; print_int value#get_is_in_stack ;  print_string " Time slice    " ; value#display_timeslice ;  print_string "\n")fu#get_hast;;
-Hashtbl.iter(fun key value ->  print_string key ; print_string " " ; print_string " " ; print_int value#get_is_in_stack ;  print_string "\n" )fu#get_hast;;
 
       
      
@@ -115,13 +97,16 @@ Hashtbl.iter(fun key value ->  print_string key ; print_string " " ; print_strin
 (* Grouping the test cases together *)
 let f1 =
 "function 1">:::
- ["nb of  functions in the program ">:: test1;
-  "nb variables in function Sum">:: test2;
-  "nb variables un function main">:: test5;
-  "Variables on registers for function f">:: test3;
-  "Variables on the stack for function Sum">:: test4;
-  "Variables on registers for function main">:: test6;
-  "Variables on the stack for function main">:: test7]
+ ["checking the number of  functions stored in the program ">:: test1;
+  "Checking the number of  variables stored in function Sum">:: test2;
+  "Checking the number of  variables stored in function Diff">:: test8;
+  "Checking the number of  variables stored in function main">:: test5;
+  "Checking the number of  variables stored in registers for function Sum">:: test3;
+  "Checking the number of  variables stored in the stack for function Sum">:: test4;
+  "Checking the number of  variables stored in the stack for function Diff">:: test9;
+  "Checking the number of  variables stored in on the stack for function Diff">:: test10;
+  "Checking the number of  variables stored in on registers for function main">:: test6;
+  "Checking the number of  variables stored in on the stack for function main">:: test7]
 ;;
 
 let () =
